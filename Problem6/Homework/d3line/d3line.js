0293a3a5f5ab926f4d400f0ec8f545efa6c3a07b
tijgerkaars <jobberbekkers@hotmail.com>
2016-12-10 21:42:52 +0100

    done

--- Problem6/Homework/d3line/d3line.js
+++ Problem6/Homework/d3line/d3line.js
@@ -118,6 +118,87 @@ window.onload = function() {
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
 
+    var mouseG = svg.append("g")
+        .attr("class", "mouse-over-effects");
+
+    mouseG.append("path")
+          .attr("class", "mouse-line")
+          .style("stroke", "black")
+          .style("stroke-width", "1px")
+          .style("opacity", "0")
+
+    var mousePerLine = mouseG.selectAll(".mousePerLine")
+                              .data(Bilt)
+                              .enter()
+                              .append("g")
+                              .attr("class", "mouse-per-line");
+    mousePerLine.append("circle")
+                .attr("r", 7)
+                .style("fille", "none")
+                .style("stroke-width", "1px")
+                .style("opacity", "0")
+    mousePerLine.append("text")
+                .attr("transform", "translate(10,3)");
+
+    mouseG.append("svg:rect")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("fill", "none")
+          .attr("pointer-events", "all")
+          .on("mouseout", function() {
+            d3.select(".mouse-line")
+              .style("opacity", "0")
+            d3.selectAll(".mouse-per-line circle")
+              .style("opacity", "0")
+            d3.selectAll(".mouse-per-line text")
+              .style("opacity", "0")
+            })
+          .on("mouseover", function () {
+            d3.select(".mouse-line")
+              .style("opacity", "1")
+            d3.selectAll(".mouse-per-line circle")
+              .style("opacity", "1")
+            d3.selectAll(".mouse-per-line text")
+              .style("opacity", "1")
+          })
+          .on("mousemove", function() {
+            var mouse = d3.mouse(this);
+            d3.select(".mouse-line")
+              .attr("d", function() {
+                var d = "M" + mouse[0] + "," + height;
+                d += " " + mouse[0] + "," + 0;
+                return d;
+              });
+          d3.selectAll(".mouse-per-line")
+            .attr("transform", function(d, i) {
+              console.log(width/mouse[0])
+              var xDate = x.invert(mouse[0]),
+                  bisect = d3.bisector(function(d) {return d.dates}).right;
+                  idx = bisect(d.TX, xDate);
+              var beginning = 0,
+                  end = lines[i].getTotalLength(),
+                  target = null;
+              while (true){
+                target = Math.floor((beginning + end) / 2);
+                pos = lines[i].getPointAtLength(target);
+                if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                  break;
+                }
+                if (pos.x > mouse[0]) {
+                  end = target;
+                }
+                else if (pos.x < mouse[0]){
+                  beginning = target;
+                }
+                else {break}
+
+                d3.select(this).select("text")
+                  .text(y.invert(pos.y).toFixed(2));
+                return "translate(" + mouse[0] + "," + pos.y + ")";
+              }
+            })
+          })
+
     // creat a canvas for the chart
     var chart = d3.select("svg")
         .attr("width", width + margin.left + margin.right)
@@ -172,6 +253,8 @@ window.onload = function() {
           .style("stroke-width", "1")
           .style("shape-rendering", "crispEdges")
           .style("opacity", "0.4")
+
+    var lines = document.getElementsByClassName('line');
 
     var path = 0;
     buttonText = d3.select("button")
